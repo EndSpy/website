fce1f30cfc51dca89d49011d8916aec9b6010ea0
EndSpy <89605456+EndSpy@users.noreply.github.com>
2022-02-28 20:50:38 -0500

    removed timer system, added a background changer instead

--- behavior.js
+++ behavior.js
@@ -1,9 +1,7 @@
 function revealMessage() {
-    document.getElementById("hiddenMessage").style.display = 'block';
+    document.getElementById("idBgColor").style.display = 'block';
 }
 
-function countDown() {
-    var currentVal = document.getElementById("countDownButton").innerHTML;
-    var newVal = currentVal - 1;
-    document.getElementById("countDownButton").innerHTML = newVal;
+function BgColor() {
+    changeBgColor
 }
