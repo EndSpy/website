f7ee15a4bc1d744b10dac7bd3de2b0c29b5ced2d
EndSpy <89605456+EndSpy@users.noreply.github.com>
2022-02-28 20:07:36 -0500

    more changes

--- behavior.js
+++ behavior.js
@@ -1,3 +1,9 @@
 function buttonpress() {
     document.getElementById("revealedmessage").style.display = 'block';
 }
+
+function buttonpress() {
+    var currentVal = document.getElementById("countdownbutton").innerHTML;
+    var newVal = currentVal - 1;
+    document.getElementById("countdownbutton").innerHTML = newVal;
+}
