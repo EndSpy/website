eaa757b89aa98c15cb164a681e37f3ba5d0bb50e
Peter Wissa <89605456+PeterWissa@users.noreply.github.com>
2022-12-17 02:27:22 -0500

    made the contact form send the email to my inbox

---
+++ contact.php
@@ -0,0 +1,20 @@
+<?php 
+
+if(isset($_POST[SUBMIT])) {
+$name = $_POST['name'];
+$mailFrom = $_POST['mail'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+
+$mailTo = "peter.wissa@icloud.com";
+$headers = "From: ".mailFrom
+$txt = "You have received an e-mail from ".$name."."\n\n".$message;
+
+
+
+mail($mailTo, $subject, $txt, $headers);
+
+header("Location: index.php?mailsend");
+
+}
