0e9eaf9fbaa4663846159c4bcf34afeb682f4a3b
Peter Wissa <89605456+PeterWissa@users.noreply.github.com>
2023-05-06 02:41:51 -0400

    bug fixes

--- behavior.js
+++ behavior.js
@@ -1,3 +1,37 @@
+
+//js for slideshow
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
+
+
 function revealMessage() {
     document.getElementById("hiddenMessage").style.display = 'block';
 }
@@ -41,35 +75,3 @@ function myfunction() {
 };
 
 
-
-//js for slideshow
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);}
-
-function currentSlide(n) {  showSlides(slideIndex = n);}
-
-function showSlides(n) {
-let i;
-  let slides = document.getElementsByClassName("mySlides");
-
-  let dots = document.getElementsByClassName("dot");
-
-  if (n > slides.length) {slideIndex = 1}
-
-  if (n < 1) {slideIndex = slides.length}
-
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-}
-
-for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
